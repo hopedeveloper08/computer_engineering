a491eeb914812efcf48214e94b79d4f160d57624
hopedeveloper08 <hopedeveloper08@gmail.com>
2024-06-04 19:56:10 +0330

    remove exchange

--- exchange/account/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Trader
-
-
-admin.site.register(Trader)
--- exchange/market/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import overview, currency_info, signal
-
-
-urlpatterns = [
-    path('overview/', overview, name='overview'),
-    path('currency_info/<str:symbol>/', currency_info, name='currency_info'),
-    path('signal/', signal, name='signal'),
-]
